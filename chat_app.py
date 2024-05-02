24065be003893d33667c1e1e8bec3d154e98e7e9
Akshatmittal52 <90290751+Akshatmittal52@users.noreply.github.com>
2024-05-02 22:52:27 +0530

    Update chat_app.py

--- chat_app.py
+++ chat_app.py
@@ -1,3 +1,4 @@
+'''
 #import ollama as ollama
 import streamlit as st
 from langchain_community.chat_models import ollama
@@ -38,3 +39,47 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         message = st.write_stream(model_res_generator())
         st.session_state["messages"].append({"role": "assistant", "content": message})
+'''
+import ollama
+import streamlit as st
+
+st.title("Ollama Python Chatbot")
+
+# Initialize history
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+# Initialize models
+if "model" not in st.session_state:
+    st.session_state["model"] = ""
+
+models = [model["name"] for model in ollama.list()["models"]]
+st.session_state["model"] = st.selectbox("Choose your model", models)
+
+def model_res_generator():
+    stream = ollama.chat(
+        model=st.session_state["model"],
+        messages=st.session_state["messages"],
+        stream=True,
+    )
+    for chunk in stream:
+        yield chunk["message"]["content"]
+
+# Display chat messages from history on app rerun
+for message in st.session_state["messages"]:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.text_input("What is up?"):
+    # Add latest message to history in format {role, content}
+    st.session_state["messages"].append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Chat with the model and display responses
+    with st.spinner("Thinking..."):
+        for message in model_res_generator():
+            with st.chat_message("assistant"):
+                st.markdown(message)
+                st.session_state["messages"].append({"role": "assistant", "content": message})
